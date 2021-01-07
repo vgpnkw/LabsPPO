fa713e11c9b83090a953fd78546b4e08d7663db8
Your Name <you@example.com>
2021-01-07 14:26:45 +0300

    update log in

--- LabThreePPO/app/src/main/java/com/example/labthreeppo/MainActivity.java
+++ LabThreePPO/app/src/main/java/com/example/labthreeppo/MainActivity.java
@@ -17,12 +17,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -34,38 +37,69 @@ public class MainActivity extends AppCompatActivity {
     FirebaseDatabase database;
     DatabaseReference playerRef;
 
+    private static final int RC_SIGN_IN = 123;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        editText = findViewById(R.id.editTextTextPersonName);
+//        button = findViewById(R.id.button);
+//
+//        database = FirebaseDatabase.getInstance();
+//        //if exists
+//        SharedPreferences preferences = getSharedPreferences("PREFS", 0);
+//        playerName = preferences.getString("playerName", "");
+//        if(!playerName.equals("")) {
+//            playerRef = database.getReference("players/" + playerName);
+//            addEventListener();
+//            playerRef.setValue("");
+//        }
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                playerName = editText.getText().toString();
+//                editText.setText("");
+//                if(!playerName.equals("")) {
+//                    button.setText("LOGGING");
+//                    button.setEnabled(false);
+//                    playerRef = database.getReference("players/" + playerName);
+//                    addEventListener();
+//                    playerRef.setValue("");
+//                }
+//            }
+//        });
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = findViewById(R.id.editTextTextPersonName);
-        button = findViewById(R.id.button);
-
-        database = FirebaseDatabase.getInstance();
-        //if exists
-        SharedPreferences preferences = getSharedPreferences("PREFS", 0);
-        playerName = preferences.getString("playerName", "");
-        if(!playerName.equals("")) {
-            playerRef = database.getReference("players/" + playerName);
-            addEventListener();
-            playerRef.setValue("");
-        }
-        button.setOnClickListener(new View.OnClickListener() {
+        Button btn = findViewById(R.id.btn_auth_gmail);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                playerName = editText.getText().toString();
-                editText.setText("");
-                if(!playerName.equals("")) {
-                    button.setText("LOGGING");
-                    button.setEnabled(false);
-                    playerRef = database.getReference("players/" + playerName);
-                    addEventListener();
-                    playerRef.setValue("");
-                }
+                List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.GoogleBuilder().build());
+                startActivityForResult(
+                        AuthUI.getInstance()
+                                .createSignInIntentBuilder()
+                                .setAvailableProviders(providers)
+                                .build(),
+                        RC_SIGN_IN);
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN && resultCode == RESULT_OK) {
+            startActivity(new Intent(getApplicationContext(), Menu.class));
+            finish();
+        } else {
+            Toast toast = Toast.makeText(this, "Что-то пошло не так", Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
+
     private void addEventListener() {
         //read from databae
         playerRef.addValueEventListener(new ValueEventListener() {
