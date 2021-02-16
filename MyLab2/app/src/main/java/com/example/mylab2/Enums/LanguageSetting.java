6f46918b5466a7278e37d8d2affe42a36bc51ac3
vgpnkw <62284467+vgpnkw@users.noreply.github.com>
2021-02-16 18:23:45 +0300

    Update LanguageSetting.java

--- MyLab2/app/src/main/java/com/example/mylab2/Enums/LanguageSetting.java
+++ MyLab2/app/src/main/java/com/example/mylab2/Enums/LanguageSetting.java
@@ -3,8 +3,6 @@ package com.example.mylab2.Enums;
 public enum LanguageSetting {
     EN("en"),
     RUS("ru"),
-    ENGLISH_rus("английский"),
-    ENGLISH_en("english");
 
     private final String setting;
 
